22608d4304adbe65b849522f38fedbd014de77ce
mehrdadsadeghi228 <63063456+mehrdadsadeghi228@users.noreply.github.com>
2023-08-13 13:51:44 -0700

    Update user.queies.js

---
+++ app/grphql/queries/user.queies.js
@@ -0,0 +1,63 @@
+const { userModel } = require("../../model/user.model");
+const { userTypeGraphQl } = require("../typedf/user.type");
+const createError = require("http-errors");
+
+const userResolver = {
+    type: new GraphQLList(userTypeGraphQl),
+    resolve: async () => {
+        const AuthorValue = await userModel.find();
+        return AuthorValue
+    }
+
+
+};
+
+const AddUserResolver = {
+    type: new GraphQLList(userTypeGraphQl),
+
+    args: {
+        userName: { type: GraphQLString },
+        name: { type: GraphQLString },
+        lastName: { type: GraphQLString },
+        email: { type: GraphQLString },
+        favorite_author: { type: new GraphQLList(AuthorBlogs) },
+        favorite_Book: { type: new GraphQLList(BookType) },
+        Comment: { type: new GraphQLList(commentTypeGraphQl) }
+    },
+
+    resolve: async (_, args) => {
+        const { userName,name, last_name, email } = args
+        const user=await userModel.find({userName:userName});
+        if(user) throw new createError.BadRequest(" Client is exist !");
+        const userValue = await userModel.create({
+            userName:userName,name:name,lastName:last_name,email:email
+        });
+
+        return userValue._id
+    }
+
+
+};
+
+
+const AddUserFavoriteBookResolver = {
+    type: new GraphQLList(userTypeGraphQl),
+
+    args: {
+        userName: { type: GraphQLString },
+        id: { type: GraphQLString },
+        favorite_author: { type: new GraphQLList(AuthorBlogs) },
+        favorite_Book: { type: new GraphQLList(BookType) },
+    },
+
+    resolve: async (_, args) => {
+        const { favorite_Book,favorite_author,userName,id } = args
+        const user=await userModel.find( { { userName:{$regex : userName } } , { _id:{$regex : id } } } );
+        if(!user) throw new createError.BadRequest(" Client is exist !");
+        const userValue = await userModel.findByIdAndUpdate({_id{$regex : title}});
+
+        return userValue
+    }
+
+
+};
